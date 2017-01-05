aededa7cd3849f979615c58ea56b7c349bbad438
Tom LaZelle <tomlazelle@yahoo.com>
2017-01-05 16:06:36 -0600

    adding description to assembly info

--- SystemNotifications/Properties/AssemblyInfo.cs
+++ SystemNotifications/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("SystemNotifications")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("SystemNotifications")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("SystemNotifications")]
